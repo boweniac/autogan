bbae5056535fd20c3709f1b64e68287712bf90fd
boweniac <boweniac@yeah.net>
2024-02-17 11:48:02 +0800

    Regular backup 20240217
    v2

--- apps/web_demo/frontend/next.config.js
+++ apps/web_demo/frontend/next.config.js
@@ -2,15 +2,15 @@
 const nextConfig = {
     env: {
         APP_NAME: "AI 博闻",
-        GATE_WAY_PROTOCOL: "http://",
-        GATE_WAY_HOST: "192.168.0.103",
-        GATE_WAY_PORT: "60302"
+        // GATE_WAY_PROTOCOL: "http://",
+        // GATE_WAY_HOST: "192.168.0.103",
+        // GATE_WAY_PORT: "60302"
         // GATE_WAY_PROTOCOL: "http://",
         // GATE_WAY_HOST: "192.168.50.84",
         // GATE_WAY_PORT: "60302"
-        // GATE_WAY_PROTOCOL: "https://",
-        // GATE_WAY_HOST: "nas.boweniac.top",
-        // GATE_WAY_PORT: "44403"
+        GATE_WAY_PROTOCOL: "https://",
+        GATE_WAY_HOST: "nas.boweniac.top",
+        GATE_WAY_PORT: "44403"
     },
 }
 
